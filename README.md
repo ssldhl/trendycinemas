928a10ab76de8ba882a8a751b3a3429330d6c2a2
sushilthe <sushilthe@gmail.com>
2015-07-14 12:13:50 +0545

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Trendy Cinemas
+========
+
+A mobile application that lists popular movies according to most-popular or by highest-rated according to users preferences.
---
+++ app/src/main/java/com/trendycinemas/PosterFragment.java
@@ -0,0 +1,23 @@
+package com.trendycinemas;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PosterFragment extends Fragment {
+
+    public PosterFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.poster, container, false);
+    }
+}
